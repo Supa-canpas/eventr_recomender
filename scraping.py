6eb32dac9ffda0a037898e41d128c1c805ccac20
two23115 <s231361u@st.go.tuat.ac.jp>
2024-08-21 15:51:12 +0900

    make scraping.py

---
+++ scraping.py
@@ -0,0 +1,22 @@
+import requests
+from bs4 import BeautifulSoup
+
+url = 'https://collabo-cafe.com/events/collabo/shingeki-gigo-cafe-ikebukuro2024/'
+res = requests.get(url)
+soup = BeautifulSoup(res.text, 'html.parser')
+news = soup.select_one('#main > article > section.entry-content > div.table__container > table')
+records = news.find_all('tr')
+info = []
+count = 0
+for record in records:
+    if count == 0:
+        buf = record.find('td').text
+    elif count == 3:
+        buf = record.find('a')['href']
+        info.append(buf)
+    else:
+        buf = record.find('td').text
+        info.append(buf)
+    count += 1
+print(info)
+
