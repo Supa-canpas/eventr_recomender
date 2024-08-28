3d090fe3dd4e4686b1e57bf3c2b52db4b4857884
miya33331 <135804436+miya33331@users.noreply.github.com>
2024-08-28 09:33:11 +0900

    最初にデータベースのテーブルのデータを削除する処理を追加。 クラスにまとめて可読性を改善。

--- index.php
+++ index.php
@@ -5,85 +5,12 @@
 </head>
 <body>
     <?php
-
-        function database_process($_title,$_officialsite,$_place,$_date,$_access)
-        {
-                $user='root';
-                $pass='root';
-            try {
-                
-                $db = new PDO('mysql:host=localhost;dbname=eventdatabase', $user, $pass);
-                
-                $title=$_title;
-                $officialsite=$_officialsite;
-                $place=$_place;
-                $date=$_date;
-                $access=$_access;
-    
-                // SQLクエリ作成
-                $stmt = $db->prepare("INSERT INTO event_info VALUES(?, ?, ?, ?, ?);");
-
-                $stmt->bindParam(1, $title,PDO::PARAM_STR);                
-                $stmt->bindParam(2, $officialsite,PDO::PARAM_STR);
-                $stmt->bindParam(3, $place, PDO::PARAM_STR);
-                $stmt->bindParam(4, $date, PDO::PARAM_STR);
-                $stmt->bindParam(5, $access, PDO::PARAM_STR);
-        
-                // クエリ実行
-                $res = $stmt->execute();
-        
-                // 切断
-                $db = null;
-            }
-            catch (PDOException) {
-                echo 'データベース接続失敗';
-            }
-        }
-
-        $command="python scraping.py";
-        exec($command, $output);
-
-        $title = "";
-        $officialsite = "";
-        $place = "";
-        $date = "";
-        $access = "";
-
-        $i=0;
-
-        foreach ($output as $_info) {
-            $info = iconv("Shift-JIS", "UTF-8", $_info);
-            if ($info == "separate_site") {
-                // echo "<hr>";
-                database_process($title,$officialsite,$place,$date,$access);
-                $i=0;
-                $title = "";
-                $officialsite = "";
-                $place = "";
-                $date = "";
-                $access = "";
-            } 
-            else if ($info == "separate_table") {
-                // echo "<br>";
-                $i++;
-            }
-            else if ($info === "コラボカフェ" or $info === "ポップアップストア" or $info === "原画展・展示会") {
-                // echo "<h1>";
-                // echo $info;
-                // echo "</h1>";
-                // echo "<hr>";
-            }
-            else {
-                // echo $info;
-
-                if($i==0) $title.=$info;
-                else if($i==1) $officialsite.=$info;
-                else if($i==2) $place.=$info;
-                else if($i==3) $date.=$info;
-                else if($i==4) $access.=$info;
-            }
-        }
-
+        require_once("save_data.php");
+        $save_event_info = new SaveEventInfo();
+        $save_event_info->exec_scraping_py();
+        $save_event_info->delete_database();
+        $save_event_info->save();
+        $save_event_info->disconnect();
     ?>
 </body>
 </html>	
---
+++ save_data.php
@@ -0,0 +1,86 @@
+<?php
+    class SaveEventInfo {
+        private $db;
+        private $output;
+
+        function __construct() {
+            $database_name = 'root';
+            $database_password = 'root';
+            $this->db = new PDO('mysql:host=localhost;dbname=eventdatabase', $database_name, $database_password);
+        }
+
+        function exec_scraping_py() {
+            $command="python scraping.py";
+            exec($command, $this->output);
+        }
+
+        function delete_database() {
+            try {
+                // SQLクエリ作成
+                $stmt = $this->db->prepare("DELETE FROM event_info;");
+                // クエリ実行
+                $res = $stmt->execute();
+            }
+            catch (PDOException) {
+                echo 'データベース接続失敗';
+            }
+        }
+
+        private function insert_database($title,$officialsite,$place,$date,$access) {
+            try {
+                // SQLクエリ作成
+                $stmt = $this->db->prepare("INSERT INTO event_info VALUES(?, ?, ?, ?, ?);");
+    
+                $stmt->bindParam(1, $title,PDO::PARAM_STR);                
+                $stmt->bindParam(2, $officialsite,PDO::PARAM_STR);
+                $stmt->bindParam(3, $place, PDO::PARAM_STR);
+                $stmt->bindParam(4, $date, PDO::PARAM_STR);
+                $stmt->bindParam(5, $access, PDO::PARAM_STR);
+    
+                // クエリ実行
+                $res = $stmt->execute();
+            }
+            catch (PDOException) {
+                echo 'データベース接続失敗';
+            }
+        }
+
+        function disconnect() {
+            // 切断
+            $this->db = null;
+        }
+
+        function save() {
+            $title = "";
+            $officialsite = "";
+            $place = "";
+            $date = "";
+            $access = "";
+            $i=0;
+
+            foreach ($this->output as $_info) {
+                $info = iconv("Shift-JIS", "UTF-8", $_info);
+
+                if ($info == "separate_site") {
+                    $this->insert_database($title,$officialsite,$place,$date,$access);
+
+                    $title = "";
+                    $officialsite = "";
+                    $place = "";
+                    $date = "";
+                    $access = "";
+                    $i=0;
+                } 
+                else if ($info == "separate_table") {$i++;}
+                else if ($info === "コラボカフェ" or $info === "ポップアップストア" or $info === "原画展・展示会") {}
+                else {
+                    if($i==0) $title.=$info;
+                    else if($i==1) $officialsite.=$info;
+                    else if($i==2) $place.=$info;
+                    else if($i==3) $date.=$info;
+                    else if($i==4) $access.=$info;
+                }
+            }
+        }
+    }
+?>
